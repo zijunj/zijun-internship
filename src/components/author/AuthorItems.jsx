3384a311b986068f2524defcd36061a411851650
zijunj <123190820+zijunj@users.noreply.github.com>
2026-03-11 04:40:05 -0400

    Fixed deploying issues pt.3

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -21,15 +21,11 @@ const AuthorItems = ({ authorImage, nftCollection }) => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="#" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="#" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="#">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+
+                        <i className="fa fa-facebook fa-lg"></i>
+
+                        <i className="fa fa-twitter fa-lg"></i>
+                        <i className="fa fa-envelope fa-lg"></i>
                       </div>
                     </div>
                   </div>
--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -111,15 +111,9 @@ const ExploreItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+                        <i className="fa fa-facebook fa-lg"></i>
+                        <i className="fa fa-twitter fa-lg"></i>
+                        <i className="fa fa-envelope fa-lg"></i>
                       </div>
                     </div>
                   </div>
